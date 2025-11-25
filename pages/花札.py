2ed624e98d43dfb09efacdc4065368935b143847
paru-akindo <akindo125paru@gmail.com>
2025-11-25 13:09:49 +0900

    Update 花札.py

--- pages/花札.py
+++ pages/花札.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_elements import elements, dashboard, mui
 
+st.set_page_config(layout="wide")
+
 with elements("demo"):
     layout = [
         dashboard.Item("first", 0, 0, 2, 2),
@@ -8,5 +10,5 @@ with elements("demo"):
     ]
 
     with dashboard.Grid(layout, cols=4, rowHeight=100):
-        mui.Box("first", sx={"bgcolor": "red"})
-        mui.Box("second", sx={"bgcolor": "blue"})
+        mui.Box("first", sx={"bgcolor": "red", "height": 100, "width": 100})
+        mui.Box("second", sx={"bgcolor": "blue", "height": 100, "width": 100})
