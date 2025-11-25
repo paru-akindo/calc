dcff7bee8d51c04a9879b3f37b08d4420ea4e1f8
paru-akindo <akindo125paru@gmail.com>
2025-11-25 13:11:03 +0900

    Update 花札.py

--- pages/花札.py
+++ pages/花札.py
@@ -6,9 +6,16 @@ st.set_page_config(layout="wide")
 with elements("demo"):
     layout = [
         dashboard.Item("first", 0, 0, 2, 2),
-        dashboard.Item("second", 2, 0, 2, 2),
     ]
 
-    with dashboard.Grid(layout, cols=4, rowHeight=100):
-        mui.Box("first", sx={"bgcolor": "red", "height": 100, "width": 100})
-        mui.Box("second", sx={"bgcolor": "blue", "height": 100, "width": 100})
+    with dashboard.Grid(layout, cols=2, rowHeight=150):
+        mui.Box(
+            "first",
+            sx={
+                "bgcolor": "black",   # 背景を黒に
+                "color": "red",       # 文字色を赤に
+                "height": 120,
+                "width": 120,
+                "border": "5px solid yellow"  # 太い黄色の枠線
+            }
+        )("VISIBLE")
