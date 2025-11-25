2954aee4c4f7140329bce88212fee7bf08a04a75
paru-akindo <akindo125paru@gmail.com>
2025-11-25 12:39:07 +0900

    Update 花札.py

--- pages/花札.py
+++ pages/花札.py
@@ -5,26 +5,21 @@ st.set_page_config(layout="wide")
 
 with elements("board"):
     layout = [
-        dashboard.Item("cell-0-0", 0, 0, 1, 1, isDraggable=False),
+        dashboard.Item("cell-0-0", 0, 0, 1, 1),
         dashboard.Item("card-1", 1, 0, 1, 1),
     ]
 
     with dashboard.Grid(layout, cols=2, rowHeight=130):
-        # 枠を描画
-        with dashboard.Item("cell-0-0"):
-            mui.Box(
-                sx={
-                    "border": "2px solid red",
-                    "height": "120px",
-                    "width": "90px"
-                }
-            )
+        # 枠
+        mui.Box(
+            key="cell-0-0",
+            sx={"border": "2px solid red", "height": 120, "width": 90}
+        )
 
-        # カードを描画
-        with dashboard.Item("card-1"):
-            mui.Card(sx={"width": "90px"})(
-                mui.CardMedia(
-                    image="https://raw.githubusercontent.com/paru-akindo/calc/main/image/1.png",
-                    sx={"height": 120}
-                )
+        # カード
+        mui.Card(key="card-1", sx={"width": 90})(
+            mui.CardMedia(
+                image="https://raw.githubusercontent.com/paru-akindo/calc/main/image/1.png",
+                sx={"height": 120}
             )
+        )
