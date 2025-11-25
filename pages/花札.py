26b546c4f4e8817f530f603d20e29ccad3325bb8
paru-akindo <akindo125paru@gmail.com>
2025-11-25 13:21:05 +0900

    Update 花札.py

--- pages/花札.py
+++ pages/花札.py
@@ -3,14 +3,12 @@ from streamlit_sortables import sort_items
 
 st.title("花札カード並べ替え")
 
-# 花札カード画像のリスト
 cards = [
-    '<img src="https://raw.githubusercontent.com/paru-akindo/calc/image/1.png" width="80">',
-    '<img src="https://raw.githubusercontent.com/paru-akindo/calc/image/2.png" width="80">',
-    '<img src="https://raw.githubusercontent.com/paru-akindo/calc/image/3.png" width="80">',
+    "![card1](https://raw.githubusercontent.com/paru-akindo/calc/main/image/1.png)",
+    "![card2](https://raw.githubusercontent.com/paru-akindo/calc/main/image/2.png)",
+    "![card3](https://raw.githubusercontent.com/paru-akindo/calc/main/image/3.png)",
 ]
 
-# 並べ替え可能にする
 sorted_cards = sort_items(cards, multi_containers=False)
 
 st.write("並べ替え後:", sorted_cards)
