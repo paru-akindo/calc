0d6ffe2fc69ca4175d3754ec17aa14ee75fd75bd
paru-akindo <akindo125paru@gmail.com>
2025-11-25 12:45:15 +0900

    Update 花札.py

--- pages/花札.py
+++ pages/花札.py
@@ -4,16 +4,18 @@ from streamlit_elements import elements, dashboard, mui
 st.set_page_config(layout="wide")
 
 with elements("demo"):
+    # レイアウト定義
     layout = [
         dashboard.Item("cell-0-0", 0, 0, 1, 1),
         dashboard.Item("card-1", 1, 0, 1, 1),
     ]
 
+    # Grid に渡す
     with dashboard.Grid(layout, cols=2, rowHeight=130):
-        # 枠
+        # 枠を描画（IDを最初の引数で渡す）
         mui.Box("cell-0-0", sx={"border": "2px solid red", "height": 120, "width": 90})
 
-        # カード
+        # カードを描画（IDを最初の引数で渡す）
         mui.Card("card-1", sx={"width": 90})(
             mui.CardMedia(
                 image="https://raw.githubusercontent.com/paru-akindo/calc/main/image/1.png",
